24c3ee8fa5541b5b856cd97a758f0ed438eac059
treytodd <trey.todd@gmail.com>
2024-11-12 22:35:28 -0800

    clean

---
+++ .ipynb_checkpoints/1encoding-cat-vars-lab-checkpoint.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Categorical encoding lab
+
+@author: Glenn
+"""
+
+import numpy as np
+import pandas as pd
+from scipy.stats import zscore
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+import seaborn as sns
+
+sns.set()
+sns.set_context('talk')   
+# default plot size
+rcParams['figure.figsize'] = 9,7
+
+
+#
+# read the data
+#
+df = pd.read_csv('https://raw.githubusercontent.com/grbruns/cst383/master/heart-categ.csv')
+
+#
+# problem 1
+#
+
+# What are the types (o+rdinal, nominal, discrete quantitative,
+# continuous quantitative) of each variable in the data set?
+
+# YOUR ANSWER HERE (as a comment)
+
+#
+# problem 2
+#
+
+# Modify the data set so that all variable are numeric.  Make sure
+# the encoding of categorical variables is performed correctly.
+
+# YOUR CODE HERE
+
+#
+# problem 3
+#
+
+# If you still have time, encode the categorical variables in the census data set
+# df = pd.read_csv('https://raw.githubusercontent.com/grbruns/cst383/master/1994-census-summary.csv')
+
+
+
+
+
+
+
+
+
+
